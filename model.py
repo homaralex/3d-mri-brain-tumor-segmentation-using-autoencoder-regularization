57a791f4f0f368da7ccb0cfecb93604d4483d441
alexander.rakowski <alexander.rakowski@hpi.de>
2021-07-08 16:04:47 +0200

    sampling layer;

--- model.py
+++ model.py
@@ -12,6 +12,8 @@ from tensorflow.keras.optimizers import Adam as adam
 from tensorflow.keras.models import Model
 from tensorflow_addons.layers import GroupNormalization
 
+from utils import sampling
+
 
 @gin.configurable(allowlist=['conv_3d_cls'])
 def green_block(
@@ -89,23 +91,6 @@ def green_block(
 
     out = Add(name=f'Out_{name}' if name else None)([x, inp_res])
     return out
-
-
-# From keras-team/keras/blob/master/examples/variational_autoencoder.py
-def sampling(args):
-    """Reparameterization trick by sampling from an isotropic unit Gaussian.
-    # Arguments
-        args (tensor): mean and log of variance of Q(z|X)
-    # Returns
-        z (tensor): sampled latent vector
-    """
-    z_mean, z_log_var = args
-    batch = tf.shape(z_mean)[0]
-    dim = tf.shape(z_mean)[1]
-    # by default, random_normal has mean = 0 and std = 1.0
-    epsilon = tf.random.normal(shape=(batch, dim))
-
-    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 
 def loss_gt(e=1e-8, data_format='channels_last'):
---
+++ utils.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+# From keras-team/keras/blob/master/examples/variational_autoencoder.py
+def sampling(args):
+    """Reparameterization trick by sampling from an isotropic unit Gaussian.
+    # Arguments
+        args (tensor): mean and log of variance of Q(z|X)
+    # Returns
+        z (tensor): sampled latent vector
+    """
+    z_mean, z_log_var = args
+    batch = tf.shape(z_mean)[0]
+    dim = tf.shape(z_mean)[1]
+    # by default, random_normal has mean = 0 and std = 1.0
+    epsilon = tf.random.normal(shape=(batch, dim))
+
+    return z_mean + tf.exp(0.5 * z_log_var) * epsilon
